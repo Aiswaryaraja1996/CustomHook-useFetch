d6b3b2883b340617bd9e432c8327e9abf442f8ff
Aiswaryaraja1996 <63009970+Aiswaryaraja1996@users.noreply.github.com>
2022-01-07 17:20:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import useFetch from "./hooks/useFetch";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  const { loading, isError, data, fetchRequest } = useFetch(
+    url + `&page=1`
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Custom Hook - useFetch</h2>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+      <button
+        onClick={() => setUrl(`https://api.github.com/search/users?q=${query}`)}
+      >
+        Searh
+      </button>
+      <div>{loading && "LOADING"}</div>
+      <div>
+        {!loading &&
+          data?.items?.map((item) => <div key={item.id}>{item.login}</div>)}
+      </div>
+   
     </div>
   );
 }
